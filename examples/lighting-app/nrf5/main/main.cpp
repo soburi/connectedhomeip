4aa2937638c640ebfac88469d9cfb03ece851b8d
Tokita, Hiroshi <soburi@gmail.com>
2021-09-06 01:45:53 +0900

    chipinit

--- examples/lighting-app/nrf5/main/main.cpp
+++ examples/lighting-app/nrf5/main/main.cpp
@@ -45,13 +45,26 @@ extern "C" {
 }
 #endif // CHIP_ENABLE_OPENTHREAD
 
+#if CHIP_ENABLE_OPENTHREAD
+#include <mbedtls/platform.h>
+#include <openthread/cli.h>
+#include <openthread/dataset.h>
+#include <openthread/error.h>
+#include <openthread/heap.h>
+#include <openthread/icmp6.h>
+#include <openthread/instance.h>
+#include <openthread/link.h>
+#include <openthread/platform/openthread-system.h>
+#include <openthread/tasklet.h>
+#include <openthread/thread.h>
+#endif // CHIP_ENABLE_OPENTHREAD
+
 #if NRF_LOG_ENABLED
 #include "nrf_log_backend_uart.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 #endif // NRF_LOG_ENABLED
 
-#include "chipinit.h"
 #include "nrf5/app/support/FreeRTOSNewlibLockSupport_test.h"
 #include <AppTask.h>
 #include <platform/CHIPDeviceLayer.h>
@@ -120,6 +133,84 @@ extern "C" void JLINK_MONITOR_OnPoll(void) {}
 // ================================================================================
 // Main Code
 // ================================================================================
+
+ret_code_t ChipInit()
+{
+    ret_code_t ret = CHIP_NO_ERROR;
+
+    NRF_LOG_INFO("Init CHIP stack");
+    ret = chip::Platform::MemoryInit();
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("PlatformMgr().InitChipStack() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+
+    ret = PlatformMgr().InitChipStack();
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("PlatformMgr().InitChipStack() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+
+#if CHIP_ENABLE_OPENTHREAD
+    NRF_LOG_INFO("Initializing OpenThread stack");
+
+    nrf_cc310_platform_abort_init();
+    nrf_cc310_platform_mutex_init();
+    mbedtls_platform_setup(NULL);
+
+    otSysInit(0, NULL);
+    NRF_LOG_INFO("Initializing OpenThread stack");
+
+    // Configure multiprotocol to work with BLE.
+    {
+        uint32_t retval = multiprotocol_802154_mode_set(MULTIPROTOCOL_802154_MODE_FAST_SWITCHING_TIMES);
+
+        if (retval != NRF_SUCCESS)
+        {
+            NRF_LOG_INFO("multiprotocol 15.4 failed");
+            APP_ERROR_HANDLER(CHIP_ERROR_INTERNAL);
+        }
+    }
+
+    ret = ThreadStackMgr().InitThreadStack();
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("ThreadStackMgr().InitThreadStack() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+
+    ret = ConnectivityMgr().SetThreadDeviceType(ConnectivityManager::kThreadDeviceType_Router);
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("ConnectivityMgr().SetThreadDeviceType() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+#endif // CHIP_ENABLE_OPENTHREAD
+
+    NRF_LOG_INFO("Starting CHIP task");
+    ret = PlatformMgr().StartEventLoopTask();
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("PlatformMgr().StartEventLoopTask() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+
+#if CHIP_ENABLE_OPENTHREAD
+    NRF_LOG_INFO("Starting OpenThread task");
+
+    // Start OpenThread task
+    ret = ThreadStackMgrImpl().StartThreadTask();
+    if (ret != CHIP_NO_ERROR)
+    {
+        NRF_LOG_INFO("ThreadStackMgr().StartThreadTask() failed");
+        APP_ERROR_HANDLER(ret);
+    }
+#endif // CHIP_ENABLE_OPENTHREAD
+
+    return ret;
+}
 
 int main(void)
 {
