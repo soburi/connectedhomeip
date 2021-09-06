5bd9ed6defed1e1d8113d3cc84d15e52dfa782ee
Tokita, Hiroshi <soburi@gmail.com>
2021-09-06 12:55:01 +0900

    clean

--- src/platform/nRF5/ConfigurationManagerImpl.h
+++ src/platform/nRF5/ConfigurationManagerImpl.h
@@ -2,6 +2,7 @@
  *
  *    Copyright (c) 2020 Project CHIP Authors
  *    Copyright (c) 2018 Nest Labs, Inc.
+ *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -24,8 +25,8 @@
 
 #pragma once
 
+#include "nRF5Config.h"
 #include <platform/internal/GenericConfigurationManagerImpl.h>
-#include <platform/nRF5/nRF5Config.h>
 
 namespace chip {
 namespace DeviceLayer {
@@ -74,7 +75,7 @@ private:
 /**
  * Returns the public interface of the ConfigurationManager singleton object.
  *
- * chip applications should use this to access features of the ConfigurationManager object
+ * Chip applications should use this to access features of the ConfigurationManager object
  * that are common to all platforms.
  */
 inline ConfigurationManager & ConfigurationMgr(void)
@@ -85,8 +86,8 @@ inline ConfigurationManager & ConfigurationMgr(void)
 /**
  * Returns the platform-specific implementation of the ConfigurationManager singleton object.
  *
- * chip applications can use this to gain access to features of the ConfigurationManager
- * that are specific to the ESP32 platform.
+ * Chip applications can use this to gain access to features of the ConfigurationManager
+ * that are specific to the nRF5 platform.
  */
 inline ConfigurationManagerImpl & ConfigurationMgrImpl(void)
 {
