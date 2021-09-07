4072e96bedb875b980db8e6cd50263f62450ec32
Tokita, Hiroshi <soburi@gmail.com>
2021-09-07 03:57:35 -0700

    publish

--- examples/arduino-bind/nrf5/publish_chip.sh
+++ examples/arduino-bind/nrf5/publish_chip.sh
@@ -1,3 +1,5 @@
+#set -x
+
 mkdir -p dist/cores/nRF5/nordic/external/openthread/include
 
 mkdir -p dist/cores/nRF5/nordic/external/connectedhomeip/gen/
@@ -10,13 +12,14 @@ cp `find out/release/obj/third_party/connectedhomeip/src -name '*.a'` dist/cores
 cp -rfp out/release/gen/include dist/cores/nRF5/nordic/external/connectedhomeip/gen/
 
 mv dist/cores/nRF5/nordic/external/connectedhomeip/gen/include/openthread*.h dist/cores/nRF5/nordic/external/openthread/include/
-cp third_party/connectedhomeip/.* dist/cores/nRF5/nordic/external/connectedhomeip/
-cp -r third_party/connectedhomeip/.github dist/cores/nRF5/nordic/external/connectedhomeip/
-cp -r third_party/connectedhomeip/.devcontainer dist/cores/nRF5/nordic/external/connectedhomeip/
-cp third_party/connectedhomeip/* dist/cores/nRF5/nordic/external/connectedhomeip/
+#cp third_party/connectedhomeip/.* dist/cores/nRF5/nordic/external/connectedhomeip/
+#cp -r third_party/connectedhomeip/.github dist/cores/nRF5/nordic/external/connectedhomeip/
+#cp -r third_party/connectedhomeip/.devcontainer dist/cores/nRF5/nordic/external/connectedhomeip/
+cp -r `find third_party/connectedhomeip/ -maxdepth 1 -name '.*' | grep -v '\.environment$' | grep -v .'git$'` dist/cores/nRF5/nordic/external/connectedhomeip/
+#cp third_party/connectedhomeip/* dist/cores/nRF5/nordic/external/connectedhomeip/
+cp -r `find third_party/connectedhomeip/ -maxdepth 1 -type f`  dist/cores/nRF5/nordic/external/connectedhomeip/
 
 find third_party/connectedhomeip/src/ -type f | egrep -v '\.(c|cpp)$' | sed 's/^third_party\/connectedhomeip\///'> /tmp/list.T
-#find src/ -name '*.hpp' >> /tmp/list.T
 tar cf /tmp/hoge.tar -C third_party/connectedhomeip/ -T /tmp/list.T 
 tar xf /tmp/hoge.tar -C dist/cores/nRF5/nordic/external/connectedhomeip/
 
@@ -38,15 +41,15 @@ rm -r dist/cores/nRF5/nordic/external/connectedhomeip/src/platform/qpg
 rm -r dist/cores/nRF5/nordic/external/connectedhomeip/src/platform/telink
 rm -r dist/cores/nRF5/nordic/external/connectedhomeip/src/test_driver
 
-mkdir -p dist/cores/nRF5/nordic/external/nrf_security/include/
-mkdir -p dist/cores/nRF5/nordic/external/nrf_security/config/nrf52840/config
-mkdir -p dist/cores/nRF5/nordic/external/nrf_security/lib/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/include/ dist/cores/nRF5/nordic/external/nrf_security/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/config/nrf52840/config/nrf-config.h dist/cores/nRF5/nordic/external/nrf_security/config/nrf52840/config/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/mbedtls_plat_config/  dist/cores/nRF5/nordic/external/nrf_security/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/nrf_cc310_platform/  dist/cores/nRF5/nordic/external/
-rm dist/cores/nRF5/nordic/external/nrf_cc310_platform/src/*zephyr*
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/lib/ dist/cores/nRF5/nordic/external/nrf_security/
+#mkdir -p dist/cores/nRF5/nordic/external/nrf_security/include/
+#mkdir -p dist/cores/nRF5/nordic/external/nrf_security/config/nrf52840/config
+#mkdir -p dist/cores/nRF5/nordic/external/nrf_security/lib/
+#cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/include/ dist/cores/nRF5/nordic/external/nrf_security/
+#cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/config/nrf52840/config/nrf-config.h dist/cores/nRF5/nordic/external/nrf_security/config/nrf52840/config/
+#cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/mbedtls_plat_config/  dist/cores/nRF5/nordic/external/nrf_security/
+#cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/nrf_cc310_platform/  dist/cores/nRF5/nordic/external/
+#rm dist/cores/nRF5/nordic/external/nrf_cc310_platform/src/*zephyr*
+#cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_party/NordicSemiconductor/libraries/nrf_security/lib/ dist/cores/nRF5/nordic/external/nrf_security/
 
 
 
@@ -59,13 +62,13 @@ cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/third_par
 mkdir -p dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/utils/
 mkdir -p dist/cores/nRF5/nordic/external/openthread/include/spinel
 mkdir -p dist/cores/nRF5/nordic/external/openthread/lib/nrf52840/gcc/
-cp -rfp third_party/connectedhomeip/third_party/openthread/repo/include/openthread/ dist/cores/nRF5/nordic/external/openthread/include/
 cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/src/src/*.h dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/openthread/src/core/config/ dist/cores/nRF5/nordic/external/openthread/include/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/openthread/examples/platforms/utils/*.h dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/utils/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/openthread/examples/platforms/openthread-system.h dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/openthread/doc/ot_api_doc.h dist/cores/nRF5/nordic/external/openthread/include/openthread/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/include/openthread/ dist/cores/nRF5/nordic/external/openthread/include/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/src/core/config/ dist/cores/nRF5/nordic/external/openthread/include/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/examples/platforms/utils/*.h dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/utils/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/examples/platforms/openthread-system.h dist/cores/nRF5/nordic/external/openthread/include/openthread/platform/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/doc/ot_api_doc.h dist/cores/nRF5/nordic/external/openthread/include/openthread/
 cp -rfp third_party/connectedhomeip/third_party/openthread/repo/src/lib/spinel/*.h dist/cores/nRF5/nordic/external/openthread/include/spinel/
-cp -rfp third_party/connectedhomeip/third_party/openthread/ot-nrf528xx/openthread/src/core/openthread-core-config.h dist/cores/nRF5/nordic/external/openthread/include/
+cp -rfp third_party/connectedhomeip/third_party/openthread/repo/src/core/openthread-core-config.h dist/cores/nRF5/nordic/external/openthread/include/
 cp `find out/release -name 'libopenthread*.a'` dist/cores/nRF5/nordic/external/openthread/lib/nrf52840/gcc/
 cp `find out/release -name 'libnordic*.a'` dist/cores/nRF5/nordic/external/openthread/lib/nrf52840/gcc/
